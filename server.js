2c1babf40307446a50a71b49d5ad2a26bf816e45
jasontsukamoto <jasontsukamoto@gmail.com>
2015-07-02 09:29:32 -1000

    adds username registration

--- server.js
+++ server.js
@@ -2,17 +2,29 @@ var net = require('net');
 var PORT = 6969;
 var HOST = '0.0.0.0';
 var socketArray = [];
+var socketObject = {};
 
 
 var server = net.createServer(function(client) {
   console.log('CONNECTED: ' + client.remoteAddress + ':' + client.remotePort);
+  client.write('Enter a username: ');
   socketArray.push(client);
 
+  client.setEncoding('utf8');
   client.on('data', function(data) {
-    console.log('SERVER BCAST FROM ' + client.remoteAddress + ':' + client.remotePort + ' : ' + data);
-    for (var i = 0; i < socketArray.length; i++) {
-      socketArray[i].write(client.remoteAddress + ':' + client.remotePort + ' : ' + data);
+    if(!socketObject.hasOwnProperty(client.remotePort)) {
+        var key = client.remotePort;
+        socketObject[key] = data.split('\n')[0];
     }
+    console.log('SERVER BCAST FROM ' + client.remoteAddress + ':' + client.remotePort + ' : ' + data);
+    for(var k in socketObject) {
+      if(client.remotePort.toString() === k) {
+        for(var i = 0; i < socketArray.length; i++) {
+          socketArray[i].write(socketObject[k] + ' says: ' + data);
+        }
+      }
+    }
+
   });
 
   client.on('end', function() {
